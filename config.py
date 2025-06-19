4e945641d2736a1cb12299fd8b3ce5d126271306
abijoy150 <abijoy150@gmail.com>
2025-06-20 01:39:11 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Telegram Bot Token
+BOT_TOKEN = "7751314755:AAFXmYJ2lW7xZhU7Txl1JuqCxG8LfbKmNZM"
+
+# Telegram Chat ID
+CHAT_ID = "6848807471"
+
+# Wingo Site URL (real-time 1-minute game data)
+WINGO_URL = "https://www.dmssc.net/gameResult/getPcTrend.do?type=1"
